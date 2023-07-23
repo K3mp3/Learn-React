35b898d5c2686b6bf33e222250174fe3bf314681
Melvin Kempe <k3mp314@gmail.com>
2023-07-23 11:34:32 +0200

    Handling events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  let items = ["New York", "Stockholm", "Oslo", "Copenhagen"];
-  items = [];
+  const items = ["New York", "Stockholm", "Oslo", "Copenhagen"];
+
+  const handleClick = (e: MouseEvent) => console.log(e);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
